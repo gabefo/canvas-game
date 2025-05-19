3816d1c06ee05c067e2a395042fba867e0a26b6c
Gabriel Mirand <gabrielfomiranda@gmail.com>
2025-05-19 03:39:41 -0300

    add name prop

--- src/Vehicle.ts
+++ src/Vehicle.ts
@@ -2,11 +2,14 @@ import { GameObject } from "./GameObject";
 import type { Player } from "./Player";
 
 export class Vehicle extends GameObject {
+  name: string;
   seats: (Player | null)[];
   fuel: number = 80;
 
-  constructor(seats: number) {
+  constructor(name: string, seats: number) {
     super(80, 120);
+
+    this.name = name;
     this.seats = Array(seats).fill(null);
   }
 
